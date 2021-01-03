a57323ece6617c1209350a3a99ab308efbfb6ff6
realaravinth <realaravinth@batsense.net>
2021-01-03 07:38:48 +0000

    Deploying to gh-pages from  @ a1e979f5bc1dc2473dcd56a07ab2a3569fb329a0 🚀

--- dwb/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["greet",""],["main",""]]});
--- source-files.js
+++
@@ -1,4 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["damn_vuln_blockchain"] = {"name":"","files":["asset.rs","blockchain.rs","error.rs","lib.rs","logs.rs","utils.rs"]};
-sourcesIndex["dwb"] = {"name":"","files":["main.rs"]};
-createSourceSidebar();
